d89869631100f05654349d23f9d5a4f947d6fe1f
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-01-08 16:03:21 +0100

    35 Challenges done

--- js.js
+++ js.js
@@ -441,3 +441,20 @@ function busStop1(people) {
 
 // console.log(busStop([[15,0],[5,1],[5,1]]))
 // console.log(busStop1([[15,0],[5,1],[5,1]]))
+
+// 35 Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+function sumOfMinimums(arr) {
+  const minValues = arr
+    .map((subarray) => Math.min(...subarray))
+    .reduce((element, value) => element + value);
+    return minValues
+}
+
+// console.log(
+//   sumOfMinimums([
+//     [11, 12, 14, 54],
+//     [67, 89, 90, 56],
+//     [7, 9, 4, 3],
+//     [9, 8, 6, 7],
+//   ])
+// );
