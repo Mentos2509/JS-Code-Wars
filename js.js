2bc878479059066afa9a72bf65a3702a986f330c
Patryk Makosa <makosapatryk.mk@gmail.com>
2023-04-30 10:22:55 +0200

    65

--- js.js
+++ js.js
@@ -886,3 +886,17 @@ function opposite(number) {
 
 // console.log(opposite(1));
 
+// 65 In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population greater or equal to p = 1200 inhabitants?
+
+function nbYear(p0, percent, aug, p) {
+  let population = p0;
+  let i = 0;
+  while (p > population) {
+    population =
+      Math.floor(population + (population * percent * 0.01) + aug);
+    i++;
+  }
+  return i;
+}
+
+console.log(nbYear(1500000, 0.25, 1000, 2000000));
